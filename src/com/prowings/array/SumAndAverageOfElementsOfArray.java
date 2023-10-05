449b440ad0bd69336b286bc7ae3fbc0665cbe9b9
JD <jayprowings@gmail.com>
2023-10-05 07:52:11 +0530

    sum and avg of array

---
+++ src/com/prowings/array/SumAndAverageOfElementsOfArray.java
@@ -0,0 +1,31 @@
+package com.prowings.array;
+
+public class SumAndAverageOfElementsOfArray {
+
+	public static void main(String[] args) {
+		
+		System.out.println("main method started");
+		int[] nums = {1,2,3};
+		
+		calculateSumandAverage(nums);
+		
+		System.out.println("main method ended");
+	}
+	
+	public static void calculateSumandAverage(int[] nums)
+	{
+		int sum = 0;
+		
+		for(int n : nums)
+		{
+			sum += n;
+		}
+		System.out.println("Sum of all elements is : "+ sum);
+		
+		int average = sum/nums.length;
+		
+		System.out.println("Average of all elements is : "+ average);
+		
+	}
+
+}
