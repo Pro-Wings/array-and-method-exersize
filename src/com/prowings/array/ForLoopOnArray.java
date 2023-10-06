be9ea150976bb2bebd601eb09b9d498149e032d2
JD <jayprowings@gmail.com>
2023-10-06 08:10:06 +0530

    filter second highest element from array

--- src/com/prowings/array/ForLoopOnArray.java
+++ src/com/prowings/array/ForLoopOnArray.java
@@ -1,32 +1,30 @@
 package com.prowings.array;
 
 public class ForLoopOnArray {
-	
+
 	public static void main(String[] args) {
-		
+
 		int[] numbers = new int[3];
-		
+
 		numbers[0] = 10;
 		numbers[1] = 20;
 		numbers[2] = 30;
 //		numbers[3] = 40;
 //		numbers[4] = 50;
-		
-		System.out.println("numbers[] lenght : "+ numbers.length);
-		
-		for(int i=0; i<numbers.length; i++)
-		{
+
+		System.out.println("numbers[] lenght : " + numbers.length);
+
+		for (int i = 0; i < numbers.length; i++) {
 			System.out.println(numbers[i]);
 		}
-		
+
 		System.out.println(">>>>>>>>>>>>>>>>>>>");
-		//for-each or enhanced for loop
-		
-		for(int n : numbers)
-		{
+		// for-each or enhanced for loop
+
+		for (int n : numbers) {
 			System.out.println(n);
 		}
-		
+
 	}
 
 }
