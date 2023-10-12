6c4b32911d50200da9a4208c5b49bf0f2c6e6ac6
JD <jayprowings@gmail.com>
2023-10-12 12:09:16 +0530

    12 oct

---
+++ src/com/prowings/abstraction/CalculatorImpl.java
@@ -0,0 +1,23 @@
+package com.prowings.abstraction;
+
+public class CalculatorImpl implements Calculator{
+
+	
+	public int addition(int n1, int n2) {
+		return n1 + n2;
+	}
+
+	public int substraction(int n1, int n2) {
+		return n1 - n2;
+	}
+
+	public int division(int n1, int n2) {
+		return n1 / n2;
+	}
+
+	public int multiplication(int n1, int n2) {
+		return n1 * n2;
+	}
+
+	
+}
---
+++ src/com/prowings/abstraction/I.java
@@ -0,0 +1,8 @@
+package com.prowings.abstraction;
+
+public interface I {
+	
+	public static final int i = 20;
+	
+	public void print();
+}
